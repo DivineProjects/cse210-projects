194d89b02686c4c4c42c9e745bbb402c890fe6e2
divineProjects <divinejigu@gmail.com>
2023-11-05 02:40:50 +0200

    Prep 3 changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
+// Divine Jigu
+// Stretch Challenge added
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // For Part 3, where we use a random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess_number = -1;
+        int guesses = 1;
+        string yes_no = "yes";
+
+        do
+        {
+            while (guess_number != magicNumber)
+            {
+                Console.Write("Guess a Number? ");
+                guess_number = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess_number)
+                {
+                    Console.WriteLine("Guess Higher");
+                    guesses++;
+                }
+                else if (magicNumber < guess_number)
+                {
+                    Console.WriteLine("Guess Lower");
+                    guesses++;
+                }
+                else
+                {
+                    Console.WriteLine("Awesome !!! You got it!");
+                }
+                
+            }
+        
+            Console.WriteLine($"Number of guesses: {guesses}");
+            guesses = 1;
+            guess_number = -1;
+            Console.Write("Do you want to Play again (yes / no): ");
+            yes_no = Console.ReadLine();
+        } while (yes_no =="yes" || yes_no =="Yes" || yes_no =="YES");
+
+
     }
 }
